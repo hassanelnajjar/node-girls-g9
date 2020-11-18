dbd213f70d6cccdd62aa7ba49c1cece4cc36248d
hassanelnajjar <hassansamielnajjar@gmail.com>
2020-11-18 11:09:06 +0200

    added files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const { router } = require("./router");
 const server = http.createServer(router);
-const PORT = 8300;
+const PORT = process.env.PORT || 8300;
 server.listen(PORT, () =>
 	console.log(`Connected Successfully @ http://localhost:${PORT} `)
 );
